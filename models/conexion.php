824e8128eedb6075b807ebd198eb97aa47f71ad3
diego pennisi <diegopennisi1@outlook.com>
2018-04-28 19:05:15 -0300

    base de datos modificada

--- models/conexion.php
+++ models/conexion.php
@@ -3,8 +3,8 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
     class Conexion{
     	static public  function conectar(){
     	 try {
-            $conexion = new PDO('mysql:host=localhost;dbname=u630310491_a', 'u630310491_diego', 
-            	'chavalote');
+            $conexion = new PDO('mysql:host=localhost;dbname=frigorifico', 'root', 
+            	'');
             $conexion->exec('SET CHARACTER SET ut8');
             $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conexion;
