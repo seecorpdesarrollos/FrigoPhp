a8b224eabd12a7b508bebf69d80f11ae944f0a5b
Diego Pennisi <diegopennisi@outlook.es>
2018-08-27 21:46:38 -0300

    terminado notas d/c

--- controllers/notasController.php
+++ controllers/notasController.php
@@ -1,18 +1,18 @@
-<?php 
-  
+<?php
+
      header("Access-Control-Allow-Origin: *");
      header("Access-Control-Allow-Headers:Origin, X-Requested-Withd, Content-Type, Accept");
    require_once '../models/notasModel.php';
    class CuentasController{
-  
+
     static public function getnotasCreditoController(){
-			 			  
+
 			      $respuesta = NotasModel::getNotasCreditoModel('notacredito');
 
 			       if ($respuesta) {
-			        
+
 			          echo json_encode($respuesta);
-			       
+
 			       }
 
         }
@@ -23,24 +23,26 @@
   	 	   $data = file_get_contents("php://input");
              $request = json_decode($data);
              $request = (array) $request;
-               
+
               $descripcionCredito =$request['descripcionCredito'];
               $cantidadCredito =$request['cantidadCredito'];
               $importeCredito =$request['importeCredito'];
               $idCliente =$request['idCliente'];
-            
-			 			  
+              $fechaCredito =$request['fechaCredito'];
+
+
 			      $respuesta = NotasModel::addNotasCreditoModel(
 			      	$descripcionCredito,
 			      	$cantidadCredito,
-			      	$importeCredito,			     
+			      	$importeCredito,
 			      	$idCliente,
+              $fechaCredito,
 			      	'notacredito');
 
 			       if ($respuesta) {
-			        
+
 			          echo json_encode($respuesta);
-			       
+
 			       }
 
         }
@@ -51,7 +53,7 @@
   	 	   $data = file_get_contents("php://input");
              $request = json_decode($data);
              $request = (array) $request;
-               
+
               $idNotaCredito =$request['idNotaCredito'];
               $idCliente =$request['idCliente'];
               $totalCredito =$request['totalCredito'];
@@ -59,32 +61,32 @@
               //  $idNotaCredito =9;
               // $idCliente =2;
               // $totalCredito =1;
-	  
+
 			      $respuesta = NotasModel::deleteNotasCreditoModel(
 			      	$idNotaCredito,
 			      	$idCliente,
-			      	$totalCredito,		     
+			      	$totalCredito,
 			      	'notacredito');
 
 			       if ($respuesta) {
-			        
+
 			          echo json_encode($respuesta);
-			       
+
 			       }
 
         }
         // ==================================================================0
         // notas de debito
         // =====================================================================
-     
+
           static public function getnotasDebitoController(){
-			 			  
+
 			      $respuesta = NotasModel::getNotasDebitoModel('notadebito');
 
 			       if ($respuesta) {
-			        
+
 			          echo json_encode($respuesta);
-			       
+
 			       }
 
         }
@@ -96,30 +98,32 @@
   	 	   $data = file_get_contents("php://input");
              $request = json_decode($data);
              $request = (array) $request;
-               
+
               $descripcionDebito =$request['descripcionDebito'];
               $cantidadDebito =$request['cantidadDebito'];
               $importeDebito =$request['importeDebito'];
               $nroCheque =$request['nroCheque'];
               $idCliente =$request['idCliente'];
-            
-			 			  
+              $fechaDebito =$request['fechaDebito'];
+
+
 			      $respuesta = NotasModel::addNotasDebitoModel(
 			      	$descripcionDebito,
 			      	$cantidadDebito,
 			      	$importeDebito,
-			      	$nroCheque,			     
+			      	$nroCheque,
 			      	$idCliente,
+              $fechaDebito,
 			      	'notadebito');
 
 			       if ($respuesta) {
-			        
+
 			          echo json_encode($respuesta);
-			       
+
 			       }
 
         }
- 
+
 
 
      	 static public function addNotasDebitoSinChequeController(){
@@ -127,24 +131,26 @@
   	 	   $data = file_get_contents("php://input");
              $request = json_decode($data);
              $request = (array) $request;
-               
+
               $descripcionDebito =$request['descripcionDebito'];
               $cantidadDebito =$request['cantidadDebito'];
               $importeDebito =$request['importeDebito'];
               $idCliente =$request['idCliente'];
-            
-			 			  
+              $fechaDebito =$request['fechaDebito'];
+
+
 			      $respuesta = NotasModel::addNotasDebitoSinChequeModel(
 			      	$descripcionDebito,
 			      	$cantidadDebito,
-			      	$importeDebito,	     
+			      	$importeDebito,
 			      	$idCliente,
+              $fechaDebito ,
 			      	'notadebito');
 
 			       if ($respuesta) {
-			        
+
 			          echo json_encode($respuesta);
-			       
+
 			       }
 
         }
@@ -158,32 +164,32 @@
     if(isset($_GET['id'])){
       if ($_GET['id'] == "getNotaCredito") {
         $delete = new CuentasController;
-        $delete->getnotasCreditoController();  
+        $delete->getnotasCreditoController();
       }
    }
 
     if(isset($_GET['id'])){
       if ($_GET['id'] == "addNotaCredito") {
         $delete = new CuentasController;
-        $delete->addNotasCreditoController();  
+        $delete->addNotasCreditoController();
       }
    }
 
     if(isset($_GET['id'])){
       if ($_GET['id'] == "deleteNotaCredito") {
         $delete = new CuentasController;
-        $delete->deleteNotasCreditoController();  
+        $delete->deleteNotasCreditoController();
       }
    }
 
 
    // notas de debitos
-   // 
-   // 
+   //
+   //
       if(isset($_GET['id'])){
       if ($_GET['id'] == "addNotaDebito") {
         $delete = new CuentasController;
-        $delete->addNotasDebitoController();  
+        $delete->addNotasDebitoController();
       }
    }
 
@@ -191,13 +197,13 @@
      if(isset($_GET['id'])){
       if ($_GET['id'] == "addNotaDebitoSinCheque") {
         $delete = new CuentasController;
-        $delete->addNotasDebitoSinChequeController();  
+        $delete->addNotasDebitoSinChequeController();
       }
    }
 
       if(isset($_GET['id'])){
       if ($_GET['id'] == "getNotaDebito") {
         $delete = new CuentasController;
-        $delete->getnotasDebitoController();  
+        $delete->getnotasDebitoController();
       }
    }
--- models/notasModel.php
+++ models/notasModel.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
   require_once 'conexion.php';
 
@@ -7,7 +7,7 @@
   	     static public function getNotasCreditoModel($table){
 
 
-   	   	    $sql = Conexion::conectar()->prepare("SELECT * FROM $table ta 
+   	   	    $sql = Conexion::conectar()->prepare("SELECT * FROM $table ta
               JOIN clientes cli ON ta.idCliente = cli.idCliente ORDER BY idNotaCredito DESC");
 
    	   	    if ($sql->execute()) {
@@ -23,13 +23,14 @@
          static public function addNotasCreditoModel(
               $descripcionCredito,
               $cantidadCredito,
-              $importeCredito,   
+              $importeCredito,
               $idCliente,
+              $fechaCredito,
                $tabla){
 
           // comprobamos que tenga saldo para realizar nota de credito
-          
-              $sql13 = Conexion::conectar()->prepare("SELECT saldoActual AS saldo FROM saldos 
+
+              $sql13 = Conexion::conectar()->prepare("SELECT saldoActual AS saldo FROM saldos
                   WHERE idCliente = :idCliente");
                $sql13->bindParam(':idCliente', $idCliente);
                $sql13->execute();
@@ -40,18 +41,19 @@
                    return 'noSaldo';
                  }else{
 
-          
+
                 $totalCredito = $cantidadCredito * $importeCredito;
-              $sql = Conexion::conectar()->prepare("INSERT INTO $tabla( descripcionCredito, cantidadCredito, importeCredito, totalCredito ,  idCliente)
-                VALUES(:descripcionCredito, :cantidadCredito , :importeCredito,:totalCredito, :idCliente)");
+              $sql = Conexion::conectar()->prepare("INSERT INTO $tabla( descripcionCredito, cantidadCredito, importeCredito, totalCredito ,  idCliente, fechaCredito)
+                VALUES(:descripcionCredito, :cantidadCredito , :importeCredito,:totalCredito, :idCliente, :fechaCredito)");
                 $sql->bindParam(':descripcionCredito', $descripcionCredito);
                 $sql->bindParam(':cantidadCredito', $cantidadCredito);
                 $sql->bindParam(':importeCredito', $importeCredito);
                 $sql->bindParam(':totalCredito', $totalCredito);
                 $sql->bindParam(':idCliente', $idCliente);
-               
+                $sql->bindParam(':fechaCredito', $fechaCredito);
+
                 if ($sql->execute()) {
-     
+
                  $sql3 = Conexion::conectar()->prepare("UPDATE  saldos SET  saldoActual= saldoActual- $totalCredito, saldoFinal= saldoFinal-  $totalCredito   WHERE idCliente = :idCliente");
                  $sql3->bindParam(':idCliente', $idCliente);
                   $sql3->execute();
@@ -62,7 +64,7 @@
               $sql41 = Conexion::conectar()->prepare("INSERT INTO cuentacorriente(comprobante,
                pagos,  idCliente, fecha)
                 VALUES(:comprobante, :pagos , :idCliente,:fecha)");
-                $hoy = date('y-m-d');
+                $hoy = $fechaCredito;
                 $comprobante = 1;
                 $sql41->bindParam(':comprobante',  $comprobante );
                 $sql41->bindParam(':pagos', $totalCredito);
@@ -71,37 +73,37 @@
                 $sql41->execute();
 
 
-                   $sql13 = Conexion::conectar()->prepare("SELECT MAX(idCuentaCorriente)AS id FROM cuentacorriente 
+                   $sql13 = Conexion::conectar()->prepare("SELECT MAX(idCuentaCorriente)AS id FROM cuentacorriente
                     WHERE idCliente = :idCliente");
                  $sql13->bindParam(':idCliente', $idCliente);
                  $sql13->execute();
                   $resu= $sql13->fetch();
                    $id =  $resu['id'];
-               
 
-                 $sql11 = Conexion::conectar()->prepare("UPDATE cuentacorriente 
-                   SET saldo= (SELECT saldoFinal FROM saldos WHERE idCliente=$idCliente)  
+
+                 $sql11 = Conexion::conectar()->prepare("UPDATE cuentacorriente
+                   SET saldo= (SELECT saldoFinal FROM saldos WHERE idCliente=$idCliente)
                    WHERE idCliente=:idCliente AND idCuentaCorriente = :id" );
                 $sql11->bindParam(':idCliente', $idCliente);
                 $sql11->bindParam(':id', $id);
                  $sql11->execute();
-                 
-                   $sql131 = Conexion::conectar()->prepare("SELECT MAX(idNotaCredito)AS idNotaCredito FROM notacredito 
+
+                   $sql131 = Conexion::conectar()->prepare("SELECT MAX(idNotaCredito)AS idNotaCredito FROM notacredito
                     WHERE idCliente = :idCliente");
                  $sql131->bindParam(':idCliente', $idCliente);
                  $sql131->execute();
                   $resu1= $sql131->fetch();
                    $idNotaCredito = 'Nota de Crédito' . ' ' . $resu1['idNotaCredito'];
                   $nroNotaCredito=$resu1['idNotaCredito'];
-                    $sql111 = Conexion::conectar()->prepare("UPDATE cuentacorriente 
-                   SET comprobante=  :idNotaCredito , nroNotaCredito=:nroNotaCredito 
+                    $sql111 = Conexion::conectar()->prepare("UPDATE cuentacorriente
+                   SET comprobante=  :idNotaCredito , nroNotaCredito=:nroNotaCredito
                    WHERE idCliente=:idCliente AND idCuentaCorriente = :id");
                 $sql111->bindParam(':idCliente', $idCliente);
                 $sql111->bindParam(':idNotaCredito', $idNotaCredito);
                 $sql111->bindParam(':nroNotaCredito', $nroNotaCredito);
                $sql111->bindParam(':id', $id);
                  $sql111->execute();
-            
+
                   return 'success';
                 }else{
                 return 'errorers';
@@ -115,37 +117,37 @@
 
 
          static public function deleteNotasCreditoModel(
-              $idNotaCredito,                 
+              $idNotaCredito,
               $idCliente,
-              $totalCredito,   
+              $totalCredito,
                $tabla){
 
-      
+
               $sql = Conexion::conectar()->prepare("DELETE FROM  cuentacorriente WHERE nroNotaCredito=:idNotaCredito");
                 $sql->bindParam(':idNotaCredito', $idNotaCredito);
-               
+
                 if ($sql->execute()) {
                    $sql3 = Conexion::conectar()->prepare("DELETE FROM  $tabla WHERE idNotaCredito=:idNotaCredito");
                   $sql3->bindParam(':idNotaCredito', $idNotaCredito);
                   $sql3->execute();
-               
 
-                 $sql11 = Conexion::conectar()->prepare("UPDATE saldos 
-                   SET saldoActual=saldoActual+ $totalCredito, saldoFinal=saldoFinal+$totalCredito 
+
+                 $sql11 = Conexion::conectar()->prepare("UPDATE saldos
+                   SET saldoActual=saldoActual+ $totalCredito, saldoFinal=saldoFinal+$totalCredito
                    WHERE idCliente=:idCliente");
                 $sql11->bindParam(':idCliente', $idCliente);
                  $sql11->execute();
 
-                     $sql13 = Conexion::conectar()->prepare("SELECT MAX(idCuentaCorriente)AS id FROM cuentacorriente 
+                     $sql13 = Conexion::conectar()->prepare("SELECT MAX(idCuentaCorriente)AS id FROM cuentacorriente
                   WHERE idCliente = :idCliente");
                $sql13->bindParam(':idCliente', $idCliente);
                $sql13->execute();
                 $resu= $sql13->fetch();
                  $id =  $resu['id'];
-             
 
-               $sql11 = Conexion::conectar()->prepare("UPDATE cuentacorriente 
-                 SET saldo= (SELECT saldoFinal FROM saldos WHERE idCliente=$idCliente)  
+
+               $sql11 = Conexion::conectar()->prepare("UPDATE cuentacorriente
+                 SET saldo= (SELECT saldoFinal FROM saldos WHERE idCliente=$idCliente)
                  WHERE idCliente=:idCliente AND idCuentaCorriente = :id" );
               $sql11->bindParam(':idCliente', $idCliente);
               $sql11->bindParam(':id', $id);
@@ -157,7 +159,7 @@
               }
                  $sql->close();
 
-               
+
 
     }
 
@@ -170,7 +172,7 @@
  static public function getNotasDebitoModel($table){
 
 
-            $sql = Conexion::conectar()->prepare("SELECT * FROM $table ta 
+            $sql = Conexion::conectar()->prepare("SELECT * FROM $table ta
               JOIN clientes cli ON ta.idCliente = cli.idCliente ORDER BY idNotaDebito DESC");
 
             if ($sql->execute()) {
@@ -186,27 +188,32 @@
               $descripcionDebito,
               $cantidadDebito,
               $importeDebito,
-              $nroCheque,   
+              $nroCheque,
               $idCliente,
+              $fechaDebito,
                $tabla){
 
-          // comprobamos que tenga saldo para realizar nota de credito
-          
-              $sql13 = Conexion::conectar()->prepare("SELECT nroCheque  FROM pagos 
-                  WHERE idCliente = :idCliente AND nroCheque=:nroCheque");
+          // comprobamos que tenga saldo para realizar nota de debitos
+          $totalDebito = $cantidadDebito * $importeDebito;
+
+              $sql13 = Conexion::conectar()->prepare("SELECT nroCheque  FROM pagos
+                  WHERE idCliente = :idCliente AND nroCheque=:nroCheque AND cheque=:totalDebito");
                $sql13->bindParam(':idCliente', $idCliente);
                $sql13->bindParam(':nroCheque', $nroCheque);
+               $sql13->bindParam(':totalDebito', $totalDebito);
                $sql13->execute();
                 $resu= $sql13->fetch();
                  $saldo =  $resu['nroCheque'];
 
-                  $sql131 = Conexion::conectar()->prepare("SELECT nroCheque  FROM notadebito 
-                  WHERE idCliente = :idCliente AND nroCheque=:nroCheque");
+                  $sql131 = Conexion::conectar()->prepare("SELECT nroCheque  FROM notadebito
+                  WHERE idCliente = :idCliente AND nroCheque=:nroCheque ");
                $sql131->bindParam(':idCliente', $idCliente);
                $sql131->bindParam(':nroCheque', $nroCheque);
                $sql131->execute();
                 $resu1= $sql131->fetch();
                  $saldo1 =  $resu1['nroCheque'];
+
+
 
                  if($saldo <=0){
                    return 'noSaldo';
@@ -215,19 +222,19 @@
                  }
                  else{
 
-          
-                $totalDebito = $cantidadDebito * $importeDebito;
-              $sql = Conexion::conectar()->prepare("INSERT INTO $tabla (descripcionDebito, cantidadDebito, importeDebito, totalDebito ,nroCheque,   idCliente)
-                VALUES(:descripcionDebito, :cantidadDebito , :importeDebito,:totalDebito,:nroCheque ,  :idCliente)");
+
+              $sql = Conexion::conectar()->prepare("INSERT INTO $tabla (descripcionDebito, cantidadDebito, importeDebito, totalDebito ,nroCheque,   idCliente , fechaDebito)
+                VALUES(:descripcionDebito, :cantidadDebito , :importeDebito,:totalDebito,:nroCheque ,  :idCliente, :fechaDebito)");
                 $sql->bindParam(':descripcionDebito', $descripcionDebito);
                 $sql->bindParam(':cantidadDebito', $cantidadDebito);
                 $sql->bindParam(':importeDebito', $importeDebito);
                 $sql->bindParam(':totalDebito', $totalDebito);
                 $sql->bindParam(':nroCheque', $nroCheque);
                 $sql->bindParam(':idCliente', $idCliente);
-               
+                $sql->bindParam(':fechaDebito', $fechaDebito);
+
                 if ($sql->execute()) {
-     
+
                  $sql3 = Conexion::conectar()->prepare("UPDATE  saldos SET  saldoActual= saldoActual+ $totalDebito, saldoFinal= saldoFinal+  $totalDebito   WHERE idCliente = :idCliente");
                  $sql3->bindParam(':idCliente', $idCliente);
                   $sql3->execute();
@@ -238,7 +245,7 @@
               $sql41 = Conexion::conectar()->prepare("INSERT INTO cuentacorriente(comprobante,
                entrada,  idCliente, fecha)
                 VALUES(:comprobante, :entrada , :idCliente,:fecha)");
-                $hoy = date('y-m-d');
+                $hoy = $fechaDebito;
                 $comprobante = 1;
                 $sql41->bindParam(':comprobante',  $comprobante );
                 $sql41->bindParam(':entrada', $totalDebito);
@@ -247,37 +254,37 @@
                 $sql41->execute();
 
 
-                   $sql13 = Conexion::conectar()->prepare("SELECT MAX(idCuentaCorriente)AS id FROM cuentacorriente 
+                   $sql13 = Conexion::conectar()->prepare("SELECT MAX(idCuentaCorriente)AS id FROM cuentacorriente
                     WHERE idCliente = :idCliente");
                  $sql13->bindParam(':idCliente', $idCliente);
                  $sql13->execute();
                   $resu= $sql13->fetch();
                    $id =  $resu['id'];
-               
 
-                 $sql11 = Conexion::conectar()->prepare("UPDATE cuentacorriente 
-                   SET saldo= (SELECT saldoFinal FROM saldos WHERE idCliente=$idCliente)  
+
+                 $sql11 = Conexion::conectar()->prepare("UPDATE cuentacorriente
+                   SET saldo= (SELECT saldoFinal FROM saldos WHERE idCliente=$idCliente)
                    WHERE idCliente=:idCliente AND idCuentaCorriente = :id" );
                 $sql11->bindParam(':idCliente', $idCliente);
                 $sql11->bindParam(':id', $id);
                  $sql11->execute();
-                 
-                   $sql131 = Conexion::conectar()->prepare("SELECT MAX(idNotaDebito)AS idNotaDebito FROM notadebito 
+
+                   $sql131 = Conexion::conectar()->prepare("SELECT MAX(idNotaDebito)AS idNotaDebito FROM notadebito
                     WHERE idCliente = :idCliente");
                  $sql131->bindParam(':idCliente', $idCliente);
                  $sql131->execute();
                   $resu1= $sql131->fetch();
                    $idNotaDebito = 'Nota de Débito' . ' ' . $resu1['idNotaDebito'];
                   $nroNotaDebito=$resu1['idNotaDebito'];
-                    $sql111 = Conexion::conectar()->prepare("UPDATE cuentacorriente 
-                   SET comprobante=  :idNotaDebito , nroNotaDebito=:nroNotaDebito 
+                    $sql111 = Conexion::conectar()->prepare("UPDATE cuentacorriente
+                   SET comprobante=  :idNotaDebito , nroNotaDebito=:nroNotaDebito
                    WHERE idCliente=:idCliente AND idCuentaCorriente = :id");
                 $sql111->bindParam(':idCliente', $idCliente);
                 $sql111->bindParam(':idNotaDebito', $idNotaDebito);
                 $sql111->bindParam(':nroNotaDebito', $nroNotaDebito);
                $sql111->bindParam(':id', $id);
                  $sql111->execute();
-            
+
                   return 'success';
                 }else{
                 return 'errorers';
@@ -289,29 +296,31 @@
     }
 
 
-   
+
 
   static public function addNotasDebitoSinChequeModel(
               $descripcionDebito,
               $cantidadDebito,
               $importeDebito,
               $idCliente,
+              $fechaDebito,
                $tabla){
 
-          
 
-          
+
+
                 $totalDebito = $cantidadDebito * $importeDebito;
-              $sql = Conexion::conectar()->prepare("INSERT INTO $tabla (descripcionDebito, cantidadDebito, importeDebito, totalDebito,   idCliente)
-                VALUES(:descripcionDebito, :cantidadDebito , :importeDebito,:totalDebito,  :idCliente)");
+              $sql = Conexion::conectar()->prepare("INSERT INTO $tabla (descripcionDebito, cantidadDebito, importeDebito, totalDebito,   idCliente, fechaDebito)
+                VALUES(:descripcionDebito, :cantidadDebito , :importeDebito,:totalDebito,  :idCliente, :fechaDebito)");
                 $sql->bindParam(':descripcionDebito', $descripcionDebito);
                 $sql->bindParam(':cantidadDebito', $cantidadDebito);
                 $sql->bindParam(':importeDebito', $importeDebito);
                 $sql->bindParam(':totalDebito', $totalDebito);
                 $sql->bindParam(':idCliente', $idCliente);
-               
+                $sql->bindParam(':fechaDebito', $fechaDebito);
+
                 if ($sql->execute()) {
-     
+
                  $sql3 = Conexion::conectar()->prepare("UPDATE  saldos SET  saldoActual= saldoActual+ $totalDebito, saldoFinal= saldoFinal+  $totalDebito   WHERE idCliente = :idCliente");
                  $sql3->bindParam(':idCliente', $idCliente);
                   $sql3->execute();
@@ -322,7 +331,7 @@
               $sql41 = Conexion::conectar()->prepare("INSERT INTO cuentacorriente(comprobante,
                entrada,  idCliente, fecha)
                 VALUES(:comprobante, :entrada , :idCliente,:fecha)");
-                $hoy = date('y-m-d');
+                $hoy = $fechaDebito;
                 $comprobante = 1;
                 $sql41->bindParam(':comprobante',  $comprobante );
                 $sql41->bindParam(':entrada', $totalDebito);
@@ -331,40 +340,40 @@
                 $sql41->execute();
 
 
-                   $sql13 = Conexion::conectar()->prepare("SELECT MAX(idCuentaCorriente)AS id FROM cuentacorriente 
+                   $sql13 = Conexion::conectar()->prepare("SELECT MAX(idCuentaCorriente)AS id FROM cuentacorriente
                     WHERE idCliente = :idCliente");
                  $sql13->bindParam(':idCliente', $idCliente);
                  $sql13->execute();
                   $resu= $sql13->fetch();
                    $id =  $resu['id'];
-               
 
-                 $sql11 = Conexion::conectar()->prepare("UPDATE cuentacorriente 
-                   SET saldo= (SELECT saldoFinal FROM saldos WHERE idCliente=$idCliente)  
+
+                 $sql11 = Conexion::conectar()->prepare("UPDATE cuentacorriente
+                   SET saldo= (SELECT saldoFinal FROM saldos WHERE idCliente=$idCliente)
                    WHERE idCliente=:idCliente AND idCuentaCorriente = :id" );
                 $sql11->bindParam(':idCliente', $idCliente);
                 $sql11->bindParam(':id', $id);
                  $sql11->execute();
-                 
-                   $sql131 = Conexion::conectar()->prepare("SELECT MAX(idNotaDebito)AS idNotaDebito FROM notadebito 
+
+                   $sql131 = Conexion::conectar()->prepare("SELECT MAX(idNotaDebito)AS idNotaDebito FROM notadebito
                     WHERE idCliente = :idCliente");
                  $sql131->bindParam(':idCliente', $idCliente);
                  $sql131->execute();
                   $resu1= $sql131->fetch();
                    $idNotaDebito = 'Nota de Débito' . ' ' . $resu1['idNotaDebito'];
                   $nroNotaDebito=$resu1['idNotaDebito'];
-                    $sql111 = Conexion::conectar()->prepare("UPDATE cuentacorriente 
-                   SET comprobante=  :idNotaDebito , nroNotaDebito=:nroNotaDebito 
+                    $sql111 = Conexion::conectar()->prepare("UPDATE cuentacorriente
+                   SET comprobante=  :idNotaDebito , nroNotaDebito=:nroNotaDebito
                    WHERE idCliente=:idCliente AND idCuentaCorriente = :id");
                 $sql111->bindParam(':idCliente', $idCliente);
                 $sql111->bindParam(':idNotaDebito', $idNotaDebito);
                 $sql111->bindParam(':nroNotaDebito', $nroNotaDebito);
                $sql111->bindParam(':id', $id);
                  $sql111->execute();
-            
+
                   return 'success';
                 }else{
-                return 'errorers';
+                return 'errores';
               }
                  $sql->close();
 
